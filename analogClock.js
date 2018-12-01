a16b3d5e503fcd9069352f946c380f8efddf60f5
Mario Bas <mariobas@wp.pl>
2018-12-01 15:32:35 +0100

    Adding simple drawClock() function

--- analogClock.js
+++ analogClock.js
@@ -1,8 +1,12 @@
-var canvas = document.getElementById("canvas");
-var ctx = canvas.getContext("2d");
-var radius = canvas.height / 2;
+let canvas = document.getElementById("canvas");
+let ctx = canvas.getContext("2d");
+let radius = canvas.height / 2; //obliczenie promienia zegara 
 ctx.translate(radius, radius);
-radius = radius * 0.90
-ctx.arc(0, 0, radius, 0 , 2*Math.PI);
-ctx.fillStyle = "white";
-ctx.fill();
+radius = radius * 0.90 //zmiejszenie promienia o 10% aby okrąg zawierał się w canvasie
+drawClock();
+function drawClock()
+{
+    ctx.arc(0, 0, radius, 0 , 2*Math.PI);
+    ctx.fillStyle = "white";
+    ctx.fill();
+}
