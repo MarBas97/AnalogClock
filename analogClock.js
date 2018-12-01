03475d05dfc3790e247e9edf7cf723ea45bd7ae2
Mario Bas <mariobas@wp.pl>
2018-12-02 00:02:21 +0100

    Added drawHourMarks() function and drawSecondsMarks() function.

--- analogClock.js
+++ analogClock.js
@@ -3,26 +3,31 @@ let ctx = canvas.getContext("2d");
 let radius = canvas.height / 2; //obliczenie promienia zegara 
 ctx.translate(radius, radius);
 radius = radius * 0.90 //zmiejszenie promienia o 10% aby okrąg zawierał się w canvasie
-drawClock(ctx,radius);
+let ang;
+drawClock();
+drawSecondsMarks();
+drawHourMarks();
 
-
-function drawClock(ctx,radius)
+function drawClock()
 {
+    // pole zegara
     ctx.arc(0, 0, radius, 0 , 2*Math.PI);
     ctx.fillStyle = "white";
     ctx.fill();
 
+    // środek zegara
     ctx.beginPath();
-    ctx.arc(0,0,radius*0.05,0,2*Math.PI);
-    ctx.fillStyle = '#333';
+    ctx.arc(0,0,radius*0.025,0,2*Math.PI);
+    ctx.fillStyle = '#456';
     ctx.fill();  
 
-    var ang;
-    var num;
-    ctx.font = radius*0.15 + "px arial";
+    // cyfry zegara
+    
+    
+    ctx.font = radius*0.1 + "px arial";
     ctx.textBaseline="middle";
     ctx.textAlign="center";
-    for(num= 1; num < 13; num++){
+    for(let num= 1; num < 13; num++){
         ang = num * Math.PI / 6;
         ctx.rotate(ang);
         ctx.translate(0, -radius*0.85);
@@ -31,5 +36,50 @@ function drawClock(ctx,radius)
         ctx.rotate(ang);
         ctx.translate(0, radius*0.85);
         ctx.rotate(-ang);
+    }   
+
+    
+
+}
+
+function drawSecondsMarks() {
+
+    for (var i = 0; i < 60; i++) {
+        ang = (i - 3) * (Math.PI * 2) / 60;       
+        ctx.lineWidth = 1;            
+        ctx.beginPath();
+
+        let x1 =  Math.cos(ang) * (radius);
+        let y1 =  Math.sin(ang) * (radius);
+        let x2 =  Math.cos(ang) * (radius - (radius / 30));
+        let y2 =  Math.sin(ang) * (radius - (radius / 30));
+
+        ctx.moveTo(x1, y1);
+        ctx.lineTo(x2, y2);
+
+        ctx.strokeStyle = '#C4D1D5';
+        ctx.stroke();
     }
 }
+function drawHourMarks() {
+
+    for (var i = 0; i < 12; i++) {
+        ang = (i - 3) * (Math.PI * 2) / 12;       
+        ctx.lineWidth = 1.5;            
+        ctx.beginPath();
+
+        let x1 =   Math.cos(ang) * (radius);
+        let y1 =   Math.sin(ang) * (radius);
+        let x2 =   Math.cos(ang) * (radius - (radius / 15));
+        let y2 =   Math.sin(ang) * (radius - (radius / 15));
+
+        ctx.moveTo(x1, y1);
+        ctx.lineTo(x2, y2);
+
+        ctx.strokeStyle = '#466B76';
+        ctx.stroke();
+    }
+}
+
+
+
