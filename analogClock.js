262c3d836bc0eb75507ded7bc1d86ecfae618c4d
Mario Bas <mariobas@wp.pl>
2018-12-01 15:46:05 +0100

    Adding a clock center

--- analogClock.js
+++ analogClock.js
@@ -3,10 +3,17 @@ let ctx = canvas.getContext("2d");
 let radius = canvas.height / 2; //obliczenie promienia zegara 
 ctx.translate(radius, radius);
 radius = radius * 0.90 //zmiejszenie promienia o 10% aby okrąg zawierał się w canvasie
-drawClock();
-function drawClock()
+drawClock(ctx,radius);
+
+
+function drawClock(ctx,radius)
 {
     ctx.arc(0, 0, radius, 0 , 2*Math.PI);
     ctx.fillStyle = "white";
     ctx.fill();
+
+    ctx.beginPath();
+    ctx.arc(0,0,radius*0.05,0,2*Math.PI);
+    ctx.fillStyle = '#333';
+    ctx.fill();  
 }
