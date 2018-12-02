d9a11ca1f8cef2499672895ae831ffa0ffb85b8b
Mario Bas <mariobas@wp.pl>
2018-12-02 15:39:52 +0100

    Added second, minute and hour hand. drawSecondsMarks and drawHourMarks has been connected into one drawMarks() function

--- analogClock.js
+++ analogClock.js
@@ -1,16 +1,32 @@
+
 let canvas = document.getElementById("canvas");
 let ctx = canvas.getContext("2d");
-let radius = canvas.height / 2; //obliczenie promienia zegara 
-ctx.translate(radius, radius);
-radius = radius * 0.90 //zmiejszenie promienia o 10% aby okrąg zawierał się w canvasie
-let ang;
-drawClock();
-drawSecondsMarks();
-drawHourMarks();
+ctx.translate(canvas.height / 2, canvas.width/2); // ustawienie punktu 0,0 w środku canvas, skraca długość kodu i zwiększa jego czytelność
 
-function drawClock()
+setInterval(showClock, 1000);
+    
+function showClock() {
+        
+
+    let ang;
+    let radius = canvas.height / 2; //obliczenie promienia zegara 
+    let time = new Date;
+    radius = radius * 0.90 //zmiejszenie promienia o 10% aby okrąg zawierał się w canvasie
+    ctx.clearRect(-canvas.height / 2,-canvas.width/2,canvas.width,canvas.height); //teoretycznie niepotrzebe ale uzyskuje się tym lepsze efekt wizualny
+        
+    drawClockBasicFace();
+    drawMarks();
+    //drawHourMarks();
+    drawHoursHand();
+    drawMinutesHand();
+    drawSecondsHand();
+        
+
+
+function drawClockBasicFace()
 {
     // pole zegara
+    ctx.beginPath();
     ctx.arc(0, 0, radius, 0 , 2*Math.PI);
     ctx.fillStyle = "white";
     ctx.fill();
@@ -22,12 +38,11 @@ function drawClock()
     ctx.fill();  
 
     // cyfry zegara
-    
-    
     ctx.font = radius*0.1 + "px arial";
     ctx.textBaseline="middle";
     ctx.textAlign="center";
-    for(let num= 1; num < 13; num++){
+    for(let num= 1; num < 13; num++)
+    {
         ang = num * Math.PI / 6;
         ctx.rotate(ang);
         ctx.translate(0, -radius*0.85);
@@ -42,10 +57,13 @@ function drawClock()
 
 }
 
-function drawSecondsMarks() {
+function drawMarks() 
+{
+    
+    for (var i = 0; i < 60; i++) 
+    {
 
-    for (var i = 0; i < 60; i++) {
-        ang = (i - 3) * (Math.PI * 2) / 60;       
+        ang = i  * (Math.PI * 2) / 60;       
         ctx.lineWidth = 1;            
         ctx.beginPath();
 
@@ -53,17 +71,27 @@ function drawSecondsMarks() {
         let y1 =  Math.sin(ang) * (radius);
         let x2 =  Math.cos(ang) * (radius - (radius / 30));
         let y2 =  Math.sin(ang) * (radius - (radius / 30));
+        ctx.strokeStyle = '#C4D1D5';
 
+        if(i%5===0)
+        {
+            ctx.lineWidth = 1.5; 
+            x2 =   Math.cos(ang) * (radius - (radius / 15));
+            y2 =   Math.sin(ang) * (radius - (radius / 15));
+            ctx.strokeStyle = '#504C4C';
+
+        }
         ctx.moveTo(x1, y1);
         ctx.lineTo(x2, y2);
 
-        ctx.strokeStyle = '#C4D1D5';
         ctx.stroke();
     }
 }
-function drawHourMarks() {
+function drawHourMarks() 
+{
 
-    for (var i = 0; i < 12; i++) {
+    for (var i = 0; i < 12; i++) 
+    {
         ang = (i - 3) * (Math.PI * 2) / 12;       
         ctx.lineWidth = 1.5;            
         ctx.beginPath();
@@ -81,5 +109,42 @@ function drawHourMarks() {
     }
 }
 
+function drawSecondsHand()
+{
+    ctx.lineWidth = 1.5;
+    let seconds =  time.getSeconds();
+    ang = Math.PI * 2* (seconds/60) - Math.PI/2;
+    ctx.beginPath();
+    ctx.moveTo(0,0);
+    ctx.lineTo(Math.cos(ang)*radius*0.9,Math.sin(ang)*radius*0.9);
+    ctx.strokeStyle = '#586A73';        
+    ctx.stroke();
 
+}
 
+function drawMinutesHand()
+{
+    ctx.lineWidth = 2.5;
+    let minutes =  time.getMinutes();
+    ang = (Math.PI * 2* (minutes/60)) - Math.PI/2;
+    ctx.beginPath();
+    ctx.moveTo(0,0);
+    ctx.lineTo((Math.cos(ang)*radius*0.75),Math.sin(ang)*radius*0.75);
+    ctx.strokeStyle = '#586A73';        
+    ctx.stroke();
+
+}
+
+function drawHoursHand()
+{
+    ctx.lineWidth = 4;
+    let hours =  time.getHours();
+    ang = (Math.PI * 2* (hours/12)) - Math.PI/2;
+    ctx.beginPath();
+    ctx.moveTo(0,0);
+    ctx.lineTo((Math.cos(ang)*radius*0.45),Math.sin(ang)*radius*0.45);
+    ctx.strokeStyle = '#586A73';        
+    ctx.stroke();
+
+}
+}
